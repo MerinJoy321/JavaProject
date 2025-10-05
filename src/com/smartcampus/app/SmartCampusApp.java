8872f22dce8baf1b23b7bab30c4e3b26fcbbb6e5
Merin321 <jmerin2005@gmail.com>
2025-10-05 19:58:35 +0530

    Revert "Add login and sign-up pages with role-based access; improve UI responsiveness and styling"
    This reverts commit a7e46a0a135268578b3780e934adb907ac9adbc1.

--- src/com/smartcampus/app/SmartCampusApp.java
+++ src/com/smartcampus/app/SmartCampusApp.java
@@ -1,65 +1,24 @@
 package com.smartcampus.app;
 
-import com.smartcampus.data.model.User;
-import com.smartcampus.ui.view.LoginView;
 import com.smartcampus.ui.view.MainDashboard;
-import com.smartcampus.ui.view.SignUpView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SmartCampusApp extends Application {
 
-    private Stage primaryStage;
-    private User loggedInUser;
-
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
         primaryStage.setTitle("Smart Campus Navigation & Notification System");
 
-        showLoginView();
-        primaryStage.show();
-    }
-
-    private void showLoginView() {
-        LoginView loginView = new LoginView(new LoginView.LoginCallback() {
-            @Override
-            public void onLoginSuccess(User user) {
-                loggedInUser = user;
-                showMainDashboard();
-            }
-
-            @Override
-            public void onSignUpClicked() {
-                showSignUpView();
-            }
-        });
-        Scene scene = new Scene(loginView, 400, 300);
-        primaryStage.setScene(scene);
-    }
-
-    private void showSignUpView() {
-        SignUpView signUpView = new SignUpView(new SignUpView.SignUpCallback() {
-            @Override
-            public void onSignUpSuccess(User user) {
-                loggedInUser = user;
-                showMainDashboard();
-            }
-
-            @Override
-            public void onLoginClicked() {
-                showLoginView();
-            }
-        });
-        Scene scene = new Scene(signUpView, 400, 350);
-        primaryStage.setScene(scene);
-    }
-
-    private void showMainDashboard() {
-        MainDashboard mainDashboard = new MainDashboard(loggedInUser);
+        MainDashboard mainDashboard = new MainDashboard();
         Scene scene = new Scene(mainDashboard, 1024, 768);
+        
+        // Optional: Add a stylesheet for a more polished look
+        // scene.getStylesheets().add(getClass().getResource("/com/smartcampus/ui/resource/style.css").toExternalForm());
+
         primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
